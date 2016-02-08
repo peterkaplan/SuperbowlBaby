84250a5b60505fcbf02826b08c6f83d577bbde8a
michaelm244 <michaelm244@gmail.com>
2016-02-08 00:14:03 -0500

    bleh

--- public/birthday.js
+++ public/birthday.js
@@ -1,13 +1,20 @@
 var data =["Arizona Cardinals","Chicago Bears","Green Bay Packers","New York Giants","Detroit Lions","Washington Redskins","Philadelphia Eagles","Pittsburgh Steelers","Los Angeles Rams","San Francisco 49ers","Cleveland Browns","Indianapolis Colts","Dallas Cowboys","Kansas City Chiefs","San Diego Chargers","Denver Broncos","New York Jets","New England Patriots","Oakland Raiders","Tennessee Titans","Buffalo Bills","Minnesota Vikings","Atlanta Falcons","Miami Dolphins","New Orleans Saints","Cincinnati Bengals","Seattle Seahawks","Tampa Bay Buccaneers","Carolina Panthers","Jacksonville Jaguars","Baltimore Ravens","Houston Texans"]
 $(function() {
     $("#go").click(function() {
+        $(".info-container").hide();
+        $("#loader").show();
+        $("#loader").css("display", "block");
         var data = {
             year: $("#year").val(),
             month: $("#month").val(),
             team: $("#team").val()
         };
         $.post("/check", data, function(resp) {
-            console.log(resp);
+            if(resp.result) {
+            }
+            else {
+
+            }
         });
     });
 

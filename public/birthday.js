e6a76048fff9887e725db1299c3173607054cb8c
michaelm244 <michaelm244@gmail.com>
2016-02-08 02:23:57 -0500

    twitter/fb buttons

--- public/birthday.js
+++ public/birthday.js
@@ -1,6 +1,9 @@
 var team_names = [ "Atlanta Falcons", "Baltimore Ravens", "Buffalo Bills", "Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", "Cleveland Browns", "Dallas Cowboys", "Denver Broncos", "Detroit Lions", "Green Bay Packers", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Los Angeles Rams", "Miami Dolphins", "Minnesota Vikings", "New England Patriots", "New Orleans Saints", "New York Giants", "New York Jets", "Oakland Raiders", "Philadelphia Eagles", "Pittsburgh Steelers", "San Diego Chargers", "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans", "Washington Redskins"];
 $(function() {
-
+    setTimeout(function() {
+        $(".twitter-share-button").css('margin-bottom', '-5px');
+        $(".twitter-share-button").css('margin-left', '10px');
+    }, 1000);
 
     $('#day').keypress(function(e) {
     if (e.which == 13) {

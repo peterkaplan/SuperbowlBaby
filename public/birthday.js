1b6438bc68860c9ecdb1ea1ac124fe7e223b61bd
peterkaplan <pkap@umich.edu>
2016-02-07 23:34:58 -0500

    SDasdf

--- public/birthday.js
+++ public/birthday.js
@@ -1,3 +1,4 @@
+var data =["Arizona Cardinals","Chicago Bears","Green Bay Packers","New York Giants","Detroit Lions","Washington Redskins","Philadelphia Eagles","Pittsburgh Steelers","Los Angeles Rams","San Francisco 49ers","Cleveland Browns","Indianapolis Colts","Dallas Cowboys","Kansas City Chiefs","San Diego Chargers","Denver Broncos","New York Jets","New England Patriots","Oakland Raiders","Tennessee Titans","Buffalo Bills","Minnesota Vikings","Atlanta Falcons","Miami Dolphins","New Orleans Saints","Cincinnati Bengals","Seattle Seahawks","Tampa Bay Buccaneers","Carolina Panthers","Jacksonville Jaguars","Baltimore Ravens","Houston Texans"]
 $(function() {
     $("#go").click(function() {
         var data = {
@@ -9,5 +10,12 @@ $(function() {
             console.log(resp);
         });
     });
+
+    // add team options
+  for( index in data )
+        {
+          $('#drop').append('<li class="dropdown"><a href="#">' + data[index] +' </a></li>');
+
+        }
 });
 

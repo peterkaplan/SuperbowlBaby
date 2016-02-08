9ed8ba47ec1005b83d4a01019a02cb2b418fbd6e
michaelm244 <michaelm244@gmail.com>
2016-02-07 23:14:01 -0500

    js

--- public/birthday.js
+++ public/birthday.js
@@ -1,5 +1,13 @@
-$("#go").click(function() {
-    var data = {
-    }
-    $.post("/check")
+$(function() {
+    $("#go").click(function() {
+        var data = {
+            year: $("#year").val(),
+            month: $("#month").val(),
+            team: $("#team").val()
+        };
+        $.post("/check", data, function(resp) {
+            console.log(resp);
+        });
+    });
 });
+
